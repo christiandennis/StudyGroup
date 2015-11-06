e059c3c7c831463dde11d16c66734532265fcb2c
MohitPKohli <mkohli@berkeley.edu>
2015-11-06 11:50:47 -0800

    Added changes to the api for groups and users

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ActiveRecord::Base
 	after_initialize :init
 
     def init
-      self.host  ||= 'anonymous'           #will set the default value only if it's nil
+      self.host  ||= ''           #will set the default value only if it's nil
       self.guestlist ||= 0 #let's you set a default association
+      self.going ||='going:'
     end
 end
