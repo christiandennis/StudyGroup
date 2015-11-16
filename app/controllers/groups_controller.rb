0d48850fcb53cacbc5a2828b493d278136c0fea3
MohitPKohli <mkohli@berkeley.edu>
2015-11-15 19:23:14 -0800

    Fixed Group controller but still need to check if user is authenticated when creating groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,10 +2,13 @@ class GroupsController < ApplicationController
 	skip_before_filter :verify_authenticity_token
   	clear_respond_to
   	respond_to :json
+  	#before_action :authenticate_user!
 
 	def create
 		status = 1 #intially set status to OK
 		error_messages = [] #List of all errors
+
+		
 		
 		title = params[:title]
 		subject = params[:subject]
@@ -14,8 +17,9 @@ class GroupsController < ApplicationController
 		date = params[:date]
 		time = params[:time]
 		capacity = params[:capacity]
-		host = params[:host]
+		#host = params[:host]
 		privacy = params[:privacy]
+		uid = params[:uid]
 
 		if title.nil? || title.length==0
 			status = -1
@@ -41,31 +45,35 @@ class GroupsController < ApplicationController
 			error_messages << "Please enter description less than 256 characters" 
 		end
 
-		if host.nil?
-			status=-1
-			error_messages << "Please pass in a host id"
-		end
+		# if host.nil?
+		# 	status=-1
+		# 	error_messages << "Please pass in a host id"
+		# end
+
+		@user = nil
 
 		if privacy != '0' and privacy != '1'
 			status=-1
 			error_messages << "Please set privacy of 0 or 1"
 		end
 
-		@enduser = Enduser.find_by_id(host)
-
-		if @enduser.nil?
-			status=-1
-			error_messages << 'Could not find user with passed in host id'
+		if uid.nil?
+			status = -1
+			error_messages << "Please pass in a uid"
 		else
-			params[:host] = @enduser.username
+			@user = User.find_by_uid(uid)
+			if @user.nil?
+				status = -1
+				error_messages << "Couldn't find user with given uid"
+			end
+
+			params[:host] = uid
 		end
-
-
 
 		if status == 1
 			@group = Group.new(group_params)
 			@group.guestlist+=1
-			@group.going = @group.going + ','+host + ','
+			@group.going = @group.going + ','+@user.id.to_s + ','
 			if @group.save
 				render json: {'status' => 1, 'group' => @group}
 			end
@@ -83,14 +91,36 @@ class GroupsController < ApplicationController
 		#based on school/user
 		# what to initially show
 		@groups = Group.all
-		@enduser = Enduser.find_by_id(params[:id])
+		@user = nil
+		if params[:uid].nil?
+			render json: {'status'=>-1, 'errors'=>['Please enter a uid']}
+			return
+		else
+			@user = User.find_by_uid(params[:uid])
+			if @user.nil?
+				render json: {'status'=>-1, 'errors'=>["Can't find user with given uid"]}
+				return
+			end
+		end
+		@user = User.find_by_uid(params[:uid])
 		render json: {'status'=>1,'groups' => @groups}
 	end
 
 	def usergroups
 		@groups = Group.all
-		@enduser = Enduser.find_by_id(params[:id])
-		id_formatted = ','+params[:id]+','
+		@user=nil
+		if params[:uid].nil?
+			render json: {'status'=>-1, 'errors'=>['Please enter a uid']}
+			return
+		else
+			@user = User.find_by_uid(params[:uid])
+			if @user.nil?
+				render json: {'status'=>-1, 'errors'=>["Can't find user with given uid"]}
+				return
+			end
+		end
+		
+		id_formatted = ','+@user.id.to_s+','
 		displayed_groups = []
 
 		for group in @groups
@@ -111,13 +141,44 @@ class GroupsController < ApplicationController
 		act = params[:command]
 		groupid = params[:groupid]
 		userid = params[:userid]
-		id_formatted = ','+userid+','
+		id_formatted = nil
 
-		@group = Group.find(groupid)
+		status = 1
+		err = []
+
+		@group = nil
+		@user = nil
+
+		if act.nil?
+			status = -1
+			err << 'Please add a command that is either add or remove'
+		end
+
+		if groupid.nil?
+			status = -1
+			err << 'Please pass in a group id'
+		else
+			@group = Group.find(groupid)
+			status = -1
+			err << 'Could not find groupd with id'
+		end
+
+		if userid.nil?
+			status = -1
+			err << 'Please pass in a uid'
+		else
+			@user = User.find_by_uid(userid)
+			status = -1
+			err << 'Could not find user with uid'
+		end
+
+		if status == -1
+			id_formatted = ','+@user.id.to_s+','
+		end
 
 		if act == 'add'
 			if not @group.going.include? id_formatted
-				@group.going = @group.going+','+userid+','
+				@group.going = @group.going+','+@user.id.to_s+','
 				@group.guestlist+=1
 			end	
 		else #remove
@@ -126,7 +187,7 @@ class GroupsController < ApplicationController
 			if going.include? id_formatted and @group.guestlist>0
 				@group.guestlist-=1
 			end
-			remove_this = ','+userid+','
+			remove_this = ','+@user.id.to_s+','
 			going.slice! remove_this
 			@group.going = going
 		end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
+  # :confirmable removed to disable email auth
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :omniauthable
   include DeviseTokenAuth::Concerns::User
-  before_save -> { skip_confirmation! }
+  #before_save -> { skip_confirmation! }
 
 end
