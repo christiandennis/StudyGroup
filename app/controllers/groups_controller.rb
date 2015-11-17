9ee609a9e7a1248e29adb3e8453382b0094f2b6d
MohitPKohli <mkohli@berkeley.edu>
2015-11-17 15:12:31 -0800

    Changed hosts from uid/email to nickname

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,8 +15,8 @@ class GroupsController < ApplicationController
 		time = params[:time]
 		capacity = params[:capacity]
 		privacy = params[:privacy]
-		host = current_user.uid
-		@name = current_user.uid
+		params[:host] = current_user.nickname
+		@name = current_user
 
 		if title.nil? || title.length==0
 			status = -1
@@ -67,7 +67,7 @@ class GroupsController < ApplicationController
 		if status == 1
 			@group = Group.new(group_params)
 			@group.guestlist+=1
-			@group.going = @group.going + ','+@name.to_s + ','
+			@group.going = @group.going + ' '+@name.nickname.to_s + ' '
 			if @group.save
 				render json: {'status' => 1, 'group' => @group}
 			end
@@ -76,45 +76,29 @@ class GroupsController < ApplicationController
 		end
 	end 
 
+	#TODO: order groups by date
 	def index
 		@groups = Group.all
 		render json: {'status'=>1,'groups' => @groups}
 	end
 
+	#TODO: Order based on date
+	#TODO: Actually select things user should see
 	def userindex
 		#based on school/user
 		# what to initially show
 		@groups = Group.all
-		@user = nil
-		if params[:uid].nil?
-			render json: {'status'=>-1, 'errors'=>['Please enter a uid']}
-			return
-		else
-			@user = User.find_by_uid(params[:uid])
-			if @user.nil?
-				render json: {'status'=>-1, 'errors'=>["Can't find user with given uid"]}
-				return
-			end
-		end
-		@user = User.find_by_uid(params[:uid])
+		@user = current_user
+
 		render json: {'status'=>1,'groups' => @groups}
 	end
 
+	#TODO: Order displayed_groups by date
 	def usergroups
 		@groups = Group.all
-		@user=nil
-		if params[:uid].nil?
-			render json: {'status'=>-1, 'errors'=>['Please enter a uid']}
-			return
-		else
-			@user = User.find_by_uid(params[:uid])
-			if @user.nil?
-				render json: {'status'=>-1, 'errors'=>["Can't find user with given uid"]}
-				return
-			end
-		end
+		@user = current_user
 		
-		id_formatted = ','+@user.id.to_s+','
+		id_formatted = ' '+@user.nickname.to_s+' '
 		displayed_groups = []
 
 		for group in @groups
@@ -130,18 +114,19 @@ class GroupsController < ApplicationController
 		@group.destroy
 	end
 
+
+	#TODO?: Make sure you can't remove host
 	def addremoveuser
 		#add or remove a user
 		act = params[:command]
 		groupid = params[:groupid]
-		userid = params[:userid]
-		id_formatted = nil
-
 		status = 1
 		err = []
 
 		@group = nil
-		@user = nil
+		@user = current_user
+
+		id_formatted = id_formatted = ' '+@user.nickname.to_s+' '
 
 		if act.nil?
 			status = -1
@@ -153,26 +138,20 @@ class GroupsController < ApplicationController
 			err << 'Please pass in a group id'
 		else
 			@group = Group.find(groupid)
-			status = -1
-			err << 'Could not find groupd with id'
-		end
-
-		if userid.nil?
-			status = -1
-			err << 'Please pass in a uid'
-		else
-			@user = User.find_by_uid(userid)
-			status = -1
-			err << 'Could not find user with uid'
+			if @group.nil?
+				status = -1
+				err << 'Could not find groupd with id'
+			end
 		end
 
 		if status == -1
-			id_formatted = ','+@user.id.to_s+','
+			render json: {'status'=>-1,'errors'=>err}
+			return
 		end
 
 		if act == 'add'
 			if not @group.going.include? id_formatted
-				@group.going = @group.going+','+@user.id.to_s+','
+				@group.going = @group.going+id_formatted
 				@group.guestlist+=1
 			end	
 		else #remove
@@ -181,8 +160,7 @@ class GroupsController < ApplicationController
 			if going.include? id_formatted and @group.guestlist>0
 				@group.guestlist-=1
 			end
-			remove_this = ','+@user.id.to_s+','
-			going.slice! remove_this
+			going.slice! id_formatted
 			@group.going = going
 		end
 
@@ -211,7 +189,6 @@ class GroupsController < ApplicationController
 		location = params[:location]
 		time = params[:time]
 		capacity = params[:capacity]
-		host = params[:host]
 		privacy = params[:privacy]
 		school = params[:school]
 
@@ -236,14 +213,7 @@ class GroupsController < ApplicationController
 		if not capacity.nil?
 			@group.capacity = capacity
 		end
-		if not host.nil?
-			@user = User.find_by_uid(host)
-			if @user.nil?
-				render json: {'status'=>-1, 'errors'=>['Could not find passed in host']}
-				return
-			end
-			@group.host = host
-		end
+
 		if not privacy.nil?
 			@group.privacy = privacy
 		end
@@ -251,13 +221,12 @@ class GroupsController < ApplicationController
 			@group.school = school
 		end
 
-		@group.save
-
-		render json: {'status'=>1,'group'=>@group}
-
-
-
-
+		if @group.host != current_user.nickname
+			render json: {'status'=>-1, 'errors'=>['You can only edit group you host']}
+		else
+			@group.save
+			render json: {'status'=>1,'group'=>@group}
+		end
 	end
 
 	def show
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,6 @@ class Group < ActiveRecord::Base
     def init
       self.host  ||= ''           #will set the default value only if it's nil
       self.guestlist ||= 0 #let's you set a default association
-      self.going ||='going:'
+      self.going ||=' '
     end
 end
