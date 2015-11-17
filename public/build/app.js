9a582965aa4d5e7afd9f2cd81273603cb975bbd7
Christian Dennis <christiandennis@Christians-MacBook-Pro.local>
2015-11-17 00:35:15 -0800

    The complete signUp process now includes updating user fields. signup->login->updateUser->logout

--- public/build/app.js
+++ public/build/app.js
@@ -46530,8 +46530,7 @@ var StudyGroupSource = {
 		      		"school": schoolSignUp.getValue(),
           		"name": fullnameSignUp.getValue(),
           		"username": usernameSignUp.getValue()
-		      	}
-		      	
+		      	} 	
 		      	$.ajax({ url: '/auth',
 		      	  type: 'POST',
 		      	  beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
@@ -46541,6 +46540,98 @@ var StudyGroupSource = {
 	      	  	  console.log('response:' ,response);
 	      	  	  signUpDialog.dismiss();
 	      	  	  console.log('---------------------------------');
+
+					      var signInData = {
+					      		"email": email.getValue(),
+					      		"password": password.getValue()
+					      }
+					      $.ajax({ url: '/auth/sign_in',
+					        type: 'POST',
+					        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+					        data: signInData,
+					        success: function(data, status, xhr) {
+					        	console.log('-----------login SUCCESS-----------');
+					        	data.client = xhr.getResponseHeader('client');
+					        	data.accesstoken = xhr.getResponseHeader('access-token');
+					        	data.uid = xhr.getResponseHeader('uid');
+					          console.log('data:' ,data);
+					          console.log('---------------------------------');
+
+	          	      var updateData = {
+	          	      		"uid": xhr.getResponseHeader('uid'),
+	          	      		"access-token": xhr.getResponseHeader('access-token'),
+	          	      		"client": xhr.getResponseHeader('client'),
+	          	      		"name": fullnameSignUp.getValue(),
+	          	      		"nickname": usernameSignUp.getValue(),
+	          	      		"school": schoolSignUp.getValue()
+	          	      }
+	          	      $.ajax({ url: '/auth/set_user_fields',
+	          	        type: 'PUT',
+	          	        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+	          	        data: updateData,
+	          	        success: function(data, status, xhr) {
+	          	        	console.log('-----------updateUser SUCCESS-----------');
+	          	          console.log('data:' ,data);
+	                    	console.log('---------------------------------');
+          			      	
+          			      	$.ajax({ url: '/auth/sign_out',
+          	      	      type: 'DELETE',
+          	      	      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+          	      	      data: updateData,
+          	      	      success: function(response) {
+          	      	      	console.log('-----------signout SUCCESS-----------');
+          		      	  	  console.log('response:' ,response);
+          	      	        window.location.href = URL;
+          	      	        // history.pushState(null, '/');
+          	      	        console.log('---------------------------------');
+          	      	      },
+          	      	      error: function(response) {
+          	      	      	console.log('-----------signout FAILED-----------');
+          	      	      	// User was not found or was not logged in.
+          		      	  	  console.log('response:' ,response.responseJSON);
+          		      	  	  if (response.responseJSON.errors[0] === 'User was not found or was not logged in.') {
+          		      	  	  	window.location.href = URL;
+          		      	  	  }
+          	      	        console.log('---------------------------------');
+          	      	      }
+          	      	    }); 
+	          	        },
+	          	        error: function(response) {
+	          	        	console.log('-----------updateUser FAILED-----------');
+	          	          console.log('response:' ,response);
+	          	          console.log('---------------------------------');
+	          	          $.ajax({ url: '/auth/sign_out',
+          	      	      type: 'DELETE',
+          	      	      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+          	      	      data: updateData,
+          	      	      success: function(response) {
+          	      	      	console.log('-----------signout SUCCESS-----------');
+          		      	  	  console.log('response:' ,response);
+          	      	        console.log('---------------------------------');
+          	      	      },
+          	      	      error: function(response) {
+          	      	      	console.log('-----------signout FAILED-----------');
+          	      	      	// User was not found or was not logged in.
+          		      	  	  console.log('response:' ,response.responseJSON);
+          	      	        console.log('---------------------------------');
+          	      	      }
+          	      	    });
+	          	        }
+
+	          	      })  
+
+
+					        },
+					        error: function(response) {
+					        	console.log('-----------login FAILED-----------');
+					          console.log('response:' ,response);
+					          reject('login FAILED');
+					          console.log('---------------------------------');
+					        }
+
+					      })  	  
+					      console.log("kuda");	      	
+
 		      		},
 		      	  error: function(response) {
 		      	  	console.log('-----------signup FAILED-----------');
