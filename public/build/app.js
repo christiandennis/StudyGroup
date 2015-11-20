6390a2f213a0377a45fd7b1fbde15736ca247ad9
Dionysius Andi Hanubrata <dionysiusandihanubrata@Dionysiuss-MacBook-Pro.local>
2015-11-19 17:19:32 -0800

    Time conversion to Epoch implemented on My Groups. Done

--- public/build/app.js
+++ public/build/app.js
@@ -46165,11 +46165,10 @@ var ReactTestUtils = require('react-addons-test-utils');
 const Paper = require('material-ui/lib/paper');
 const Dialog = require('material-ui/lib/dialog');
 const FlatButton = require('material-ui/lib/flat-button');
+const moment = require('moment');
 
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
-
-const moment = require('moment');
 
 const URL = "http://localhost:3000";
 var axios = require('axios');
@@ -46179,8 +46178,11 @@ var AllSimpleGroup = React.createClass({displayName: "AllSimpleGroup",
 			return 	(
 					React.createElement("div", null, 
 					  	this.props.myGroups.map(function(myGroup, i)  {
-						  	var date = moment(myGroup.date).format("ddd, MMM D").toString();
-						  	var time = moment(myGroup.date).format("h:mm a").toString();
+					  		var d = new Date(0);
+					  		d.setUTCSeconds(myGroup.date);
+
+						  	var date = moment(d).format("ddd, MMM D").toString();
+						  	var time = moment(d).format("h:mm a").toString();
 						    return (
 						    	React.createElement("div", {key: myGroup.id}, 
 				    		        React.createElement(Paper, null, 
--- public/src/components/Dialog_MyGroups.jsx
+++ public/src/components/Dialog_MyGroups.jsx
@@ -11,11 +11,10 @@ var ReactTestUtils = require('react-addons-test-utils');
 const Paper = require('material-ui/lib/paper');
 const Dialog = require('material-ui/lib/dialog');
 const FlatButton = require('material-ui/lib/flat-button');
+const moment = require('moment');
 
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
-
-const moment = require('moment');
 
 const URL = "http://localhost:3000";
 var axios = require('axios');
@@ -25,8 +24,11 @@ var AllSimpleGroup = React.createClass({
 			return 	(
 					<div>
 					  	{this.props.myGroups.map((myGroup, i) => {
-						  	var date = moment(myGroup.date).format("ddd, MMM D").toString();
-						  	var time = moment(myGroup.date).format("h:mm a").toString();
+					  		var d = new Date(0);
+					  		d.setUTCSeconds(myGroup.date);
+
+						  	var date = moment(d).format("ddd, MMM D").toString();
+						  	var time = moment(d).format("h:mm a").toString();
 						    return (
 						    	<div key={myGroup.id}>
 				    		        <Paper>
