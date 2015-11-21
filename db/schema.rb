342e4e8a83afb905522939d59aeda968cce365ae
Theodorus Budiyanto <theodorusib@gmail.com>
2015-11-21 01:02:44 -0800

    change json layout of group

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20151121002035) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments_groups", id: false, force: :cascade do |t|
+    t.integer "group_id",   null: false
+    t.integer "comment_id", null: false
+  end
+
+  create_table "comments_users", id: false, force: :cascade do |t|
+    t.integer "user_id",    null: false
+    t.integer "comment_id", null: false
+  end
+
   create_table "endusers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -49,12 +59,10 @@ ActiveRecord::Schema.define(version: 20151121002035) do
     t.string   "school"
     t.integer  "capacity"
     t.integer  "guestlist"
-    t.text     "comments"
     t.integer  "privacy"
     t.string   "host"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "going"
   end
 
   create_table "groups_users", id: false, force: :cascade do |t|
