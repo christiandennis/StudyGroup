68efd60215713f90fef55d8a5305f2407a2a6f42
MohitPKohli <mkohli@berkeley.edu>
2015-11-20 16:53:00 -0800

    Added join table for groups_users. Update group_controller functionality accordingly. Still needs to be tested

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
 	serialize :comments, Array
 	after_initialize :init
+	has_and_belongs_to_many :users
 
     def init
       self.host  ||= ''           #will set the default value only if it's nil
