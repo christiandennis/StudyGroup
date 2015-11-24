5f444c9b0426187ac6fd6e10e5e07ed7b7e0b6da
Patrick Hong <paladin.pqh@hotmail.com>
2015-11-24 12:51:14 -0800

    Original tests (need to match types in unit tests and handle header in func tests)

--- test/controllers/comment_controller_test.rb
+++ test/controllers/comment_controller_test.rb
@@ -4,4 +4,10 @@ class CommentControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	test "should get index" do
+    	get :index
+    	assert_response :success
+  	end
+
 end
