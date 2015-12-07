22e96821fadc486257bb8de9ff5630b13af92d47
Christian Dennis <christiandennis@Christians-MacBook-Pro.local>
2015-12-06 17:53:53 -0800

    removed index (causes error)

--- db/migrate/20151115191452_devise_token_auth_create_users.rb
+++ db/migrate/20151115191452_devise_token_auth_create_users.rb
@@ -52,4 +52,6 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
+
+
 end
